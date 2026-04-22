4032a5fc1a89b072b252061ce6342c446633f47e
kou003 <kou.000333@gmail.com>
2026-04-23 01:02:01 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  base: process.env.VITE_BASE_PATH || "/",
   css: {
     modules: {
       localsConvention: "dashes",
