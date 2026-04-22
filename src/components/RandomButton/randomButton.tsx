00c25a969803e9400609b196824f47e34abfc21c
kou003 <kou.000333@gmail.com>
2026-04-23 01:21:00 +0900

    update

--- src/components/RandomButton/randomButton.tsx
+++ src/components/RandomButton/randomButton.tsx
@@ -3,11 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./styles.module.css";
 
 export function RandomButton() {
-  const seed = Math.floor(Math.random() * 10000000).toString();
+  const seed = getRandom().toString();
   const href = `#${seed}`;
   return (
     <a className={styles.randomButton} href={href} rel="noopener">
       <FontAwesomeIcon icon={faShuffle} />
     </a>
   );
+}
+
+function getRandom() {
+  const array = new Uint32Array(1);
+  crypto.getRandomValues(array);
+  return array[0];
 }
